cfbe86647c1c61cfb68d1f20183ae25ac3f4c84b
jinkanglow <61701779+jinkanglow@users.noreply.github.com>
2024-09-06 08:39:41 +0800

    Add files via upload

---
+++ DataCollection.Client/Models/ProductEntry.cs
@@ -0,0 +1,15 @@
+﻿// Models/ProductEntry.cs
+using System;
+
+namespace DataCollection.Client.Models
+{
+    public class ProductEntry
+    {
+        public string Name { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+        public string ProductModel { get; set; }
+        public string PartNumber { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ DataCollection.Client/Services/HttpClientHelper.cs
@@ -0,0 +1,22 @@
+﻿// Services/HttpClientHelper.cs
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public static class HttpClientHelper
+{
+    public static async Task<bool> IsOnline()
+    {
+        try
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("api/data");
+                return response.IsSuccessStatusCode;
+            }
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
