809913b492fdbb9806504c5014f709ac6759db1e
jinkanglow <61701779+jinkanglow@users.noreply.github.com>
2024-09-06 08:41:19 +0800

    Add files via upload

---
+++ DataCollection.Server/Startup.cs
@@ -0,0 +1,41 @@
+﻿using DataCollection.Server.Data; // Ensure this namespace is included
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace DataCollection.Server
+
+    public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddDbContext<ApplicationDbContext>();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseWebAssemblyDebugging();
+        }
+
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapRazorPages();
+            endpoints.MapFallbackToFile("index.html");
+        });
+    }
+}
